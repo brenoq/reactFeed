90871a44eedec598e028562980a7b54828a264c3
brenoq <brenoq@gmail.com>
2022-10-04 09:32:54 -0300

    Create avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
-export function Sidebar() {
+export function Sidebar() { 
   return (
     <aside className={styles.sidebar}>
       <img 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/brenoq.png" />
+        <Avatar src="https://github.com/brenoq.png"/>
 
         <strong>Breno Quirino</strong>
         <span>Web Developer</span>
